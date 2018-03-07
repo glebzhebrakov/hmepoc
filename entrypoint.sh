5be4eace1098ec09a61ba71363b7afd9cf3fa786
glib.zhebrakov <glib.zhebrakov@altexsoft.com>
2018-03-07 20:23:25 +0200

    rabbit

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ CMD="java \
     -XX:MaxHeapFreeRatio=40 \
     -XX:GCTimeRatio=4 \
     -XX:AdaptiveSizePolicyWeight=90
-    -Dspring.profiles.active=kafka \
+    -Dspring.profiles.active=rabbitmq \
     $*"
 
 echo ${CMD}
